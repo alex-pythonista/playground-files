b0f7c69bf29d1771ea82065ec9a9eb6df15a51f8
alex-pythonista <indrajit.alex@gmail.com>
2022-03-08 02:47:41 +0600

    till now

--- home.py
+++ home.py
@@ -1,17 +1,11 @@
-# to take only even index characters
-x = input('Enter a string: ')
-new_str = ""
-for i in x:
-    if x.index(i)%2 != 0:
-        new_str += i
+import functools
+lst = [3, 6, 5, 9, 2]
+ 
+# total function
+def total(x, y):
+    return x+y
 
-# converting them from lowercase to uppercase
-i = 0
-up_str = ""
-while len(new_str) > i:
-    if new_str[i]>='a' and new_str[i]<='z':
-        up_str+=chr(ord(new_str[i])-32)
-    else:
-        up_str += chr(ord(new_str[i]))
-    i+=1
-print("Uppercase string is:", up_str)
+# using reduce to compute sum of list
+result = functools.reduce(total, lst)
+
+print("The sum of the list elements is : ", result)
